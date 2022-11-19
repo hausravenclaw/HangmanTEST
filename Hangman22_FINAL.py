c2699b3b14a97c157faaf93429938a19dc13c6d6
hausravenclaw <68180572+hausravenclaw@users.noreply.github.com>
2022-11-18 20:48:51 -0500

    Update Hangman22_FINAL.py
    updated list of words

--- Hangman22_FINAL.py
+++ Hangman22_FINAL.py
@@ -35,7 +35,7 @@ HANGMAN_PICS = ['''
  /|\  |
  / \  |
      ===''']
-words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
+words = 'jacob michael joshua matthew daniel christopher andrew ethan joseph william anthony david alexander nicholas ryan tyler james john noah brandon christian dylan samuel benjamin zachary nathan logan justin gabriel jose austin kevin elijah caleb robert thomas jordan cameron jack hunter jackson angel isaiah evan isaac mason luke gavin aaron connor aiden kyle charles lucas brian adrian adam owen ian landon julian cole chase steven sebastian'.split()
 
 def getRandomWord(wordList):
     # This function returns a random string from the passed list of strings.
@@ -62,7 +62,7 @@ def displayBoard(missedLetters, correctLetters, secretWord):
     print()
 
 def getGuess(alreadyGuessed):
-    # Returns the letter the player entered. This function makes sure the player entered a single letter and not something else.
+    # Returns the entered letter. Also makes sure the player entered a letter and not a different character
     while True:
         print('Guess a letter.')
         guess = input()
@@ -91,13 +91,13 @@ gameIsDone = False
 while True:
     displayBoard(missedLetters, correctLetters, secretWord)
 
-    # Let the player enter a letter.
+    # Player can enter a letter
     guess = getGuess(missedLetters + correctLetters)
 
     if guess in secretWord:
         correctLetters = correctLetters + guess
 
-        # Check if the player has won.
+        # Check if the player has won
         foundAllLetters = True
         for i in range(len(secretWord)):
             if secretWord[i] not in correctLetters:
@@ -115,7 +115,7 @@ while True:
             print('You have run out of guesses!\nAfter ' + str(len(missedLetters)) + ' missed guesses and ' + str(len(correctLetters)) + ' correct guesses, the word was "' + secretWord + '"')
             gameIsDone = True
 
-    # Ask the player if they want to play again (but only if the game is done).
+    # Upon completion of game, ask player if they want to play again
     if gameIsDone:
         if playAgain():
             missedLetters = ''
